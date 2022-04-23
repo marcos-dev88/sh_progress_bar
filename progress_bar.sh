e27b0e92cbeaecf3b118054a426c6a7dbec405a1
marcos-dev88 <mv.piesske@gmail.com>
2022-04-22 22:42:13 -0300

    updating progress_bar code

--- progress_bar.sh
+++ progress_bar.sh
@@ -13,24 +13,27 @@ light_grey_color='\e[37m'
 
 BAR="█"
 
-inc_bar_item="$green_color█"
-left_bar_item="$light_grey_color█"
+bar_loaded_length=4
+bar_unloaded_length=$(($bar_loaded_length * 10))
 
-total_size=100
+inc_bar_item="$green_color█"
+unloading_status_item="$light_grey_color█"
+
+total_size=40
 size_value_counter=0
 
 progress_bar() {
 	percent_bar=$(("$1*100/${total_size}*100"/100))
 
-	progress=$(("${percent_bar}*4"/10))
+	loaded_bar=$(("${percent_bar}*${bar_loaded_length}"/10))
+ 
+	unloaded_bar=$(($bar_unloaded_length-$loaded_bar))
+	
+	loading_status=$(printf "%${loaded_bar}s")
 
-	remain=$((40-progress))
+	unloading_status=$(printf "%${unloaded_bar}s")
 
-	completed=$(printf "%${progress}s")
-
-	left_bar=$(printf "%${remain}s")
-
-	echo -ne "Building... ${completed// /$inc_bar_item}${BAR:i++%${#BAR}:1}${left_bar// /$left_bar_item} (${percent_bar}%)\r"
+	echo -ne "Building... ${loading_status// /$inc_bar_item}${BAR:i++%${#BAR}:1}${unloading_status// /$unloading_status_item} (${percent_bar}%)\r"
 }
 
 # To increment data in total_size variable, you can use a while loop to increment while it's going to /dev/null
@@ -42,7 +45,7 @@ while [[ ${size_value_counter} -lt ${total_size} ]] ; do
 	sleep .1
 	size_value_counter=$(($size_value_counter+1))
 	if [[ "${size_value_counter}" == "${total_size}" ]]; then
-		BAR="*"
+		BAR="█"
 	fi
 
 	progress_bar $size_value_counter
